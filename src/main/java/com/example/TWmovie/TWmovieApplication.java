2af190ca04709baaea80787f4cd41d6d3a55f8c1
Asuu <suqinzheng_0624@163.com>
2020-04-13 14:27:20 +0800

    [Asu] feat: new a SpringBoot

---
+++ src/main/java/com/example/TWmovie/TWmovieApplication.java
@@ -0,0 +1,13 @@
+package com.example.TWmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TWmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TWmovieApplication.class, args);
+	}
+
+}
